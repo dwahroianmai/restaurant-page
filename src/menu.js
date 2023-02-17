82587836b4cce00dc5e6acd292fa980d6150cf5f
dwahroianmai <105296071+dwahroianmai@users.noreply.github.com>
2023-02-17 17:08:51 +0100

    restart the project

--- src/menu.js
+++
@@ -1,7 +0,0 @@
-export function menu() {
-  const background = document.createElement("div");
-  background.setAttribute("id", "menu-block");
-  background.setAttribute("class", "block");
-
-  return background;
-}
