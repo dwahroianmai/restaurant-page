a14cdd56210bd6775f62d6758e122c93bde6a862
dwahroianmai <105296071+dwahroianmai@users.noreply.github.com>
2023-02-18 19:12:56 +0100

    add contact us tab

--- src/contactUs.js
+++ src/contactUs.js
@@ -1,6 +1,35 @@
 function contactTab() {
-  console.log("hi");
-  return;
+  const background = document.createElement("div");
+  background.setAttribute("id", "contact-block");
+  background.setAttribute("class", "block");
+
+  const heading = document.createElement("h2");
+  heading.textContent = "Make a reservation";
+  background.appendChild(heading);
+
+  const form = document.createElement("form");
+  const name = document.createElement("input");
+  name.setAttribute("placeholder", "Full name");
+  form.appendChild(name);
+  const email = document.createElement("input");
+  email.setAttribute("type", "email");
+  email.setAttribute("placeholder", "myname@email.com");
+  form.appendChild(email);
+  const date = document.createElement("input");
+  date.setAttribute("type", "datetime-local");
+  form.appendChild(date);
+  const note = document.createElement("textarea");
+  note.setAttribute("placeholder", "Your note here");
+  note.setAttribute("rows", "6");
+  note.setAttribute("cols", "43");
+  form.appendChild(note);
+  const submit = document.createElement("button");
+  submit.textContent = "Make a reservation";
+  form.appendChild(submit);
+
+  background.appendChild(form);
+
+  return background;
 }
 
 export default contactTab;
